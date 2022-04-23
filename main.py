e56379aa9e6da8055c0d31efffd43933daacd937
tato <buzondaniel@gmail.com>
2022-04-22 20:48:42 -0500

    Se creo archivo main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola")
+from Calculadora import Calculadora
+
+#Utilizar la clase Calculadora
+n1=int(input("Ingrese el primer numero: "))
+n2=int(input("Ingrese el segundo numero: "))
+calculadora = Calculadora(n1,n2)
+
+#Llamar al metodo sumar
+print(calculadora.sumar())
+
